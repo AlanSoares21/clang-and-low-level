8a7f7c05f71504824ed2df1a1f25e0d4420fca44
AlanSoares21 <alansoares26@outlook.com>
2023-09-21 20:28:40 -0300

    solving read word

--- ex-2.7/exec.asm
+++ ex-2.7/exec.asm
@@ -6,7 +6,14 @@ value: db 'abcdefghij', 0
 section .text
 global _start
 _start:
-	mov rdi, -12345234121
-	call print_int
+	lea rdi, [value]
+	mov rsi, 10
+	call read_word
+	test rax, rax
+	jz .return_error
 	mov rdi, rax
+	call print_string
 	call exit
+	.return_error:
+		mov rdi, 1
+		call exit
