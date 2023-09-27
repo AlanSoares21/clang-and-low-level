876fe9b699971d321549ad6eadbec09eb02e10f3
AlanSoares21 <alansoares26@outlook.com>
2023-09-26 23:10:24 -0300

    solving print uint

--- ex-2.7/exec.asm
+++ ex-2.7/exec.asm
@@ -2,18 +2,13 @@
 
 section .data
 value: db 'abcdefghij', 0
+numero: db '1234567890', 0
 
 section .text
 global _start
 _start:
-	lea rdi, [value]
-	mov rsi, 10
-	call read_word
-	test rax, rax
-	jz .return_error
+	lea rdi, [numero]
+	call parse_uint
 	mov rdi, rax
-	call print_string
+	call print_uint
 	call exit
-	.return_error:
-		mov rdi, 1
-		call exit
