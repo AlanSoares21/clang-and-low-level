47c3b0bc181a04b0f46e6a5a11abb90b3433e1ed
AlanSoares21 <alansoares26@outlook.com>
2023-10-07 23:29:19 -0300

    solving string copy

--- ex-2.7/exec.asm
+++ ex-2.7/exec.asm
@@ -2,14 +2,18 @@
 
 section .data
 value: db 'abc', 0
+value2: times 10 db 66, 0
 numero: db 'abc', 0
 
 section .text
 global _start
 _start:
-	lea rdi, [numero]
-	lea rsi, [value]
-	call string_equals
+	lea rdi, [value]
+	lea rsi, [value2]
+	mov rdx, 3 
+	call string_copy
 	mov rdi, rax
 	call print_uint
+	lea rdi, [value2]
+	call print_string
 	call exit
